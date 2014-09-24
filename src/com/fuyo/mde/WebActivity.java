729df14a870c9f64d5a46d0e4450e614b9429651
Yohei FUJII <riahruoy.dev@gmail.com>
2014-09-24 18:11:07 +0900

    disable built in zoom control due to overlap with adview

--- src/com/fuyo/mde/WebActivity.java
+++ src/com/fuyo/mde/WebActivity.java
@@ -98,7 +98,7 @@ public class WebActivity extends Activity {
             
             webView = new MyWebView(this);
         	webView.setWebViewClient(new MyWebViewClient());
-        	webView.getSettings().setBuiltInZoomControls(true);
+//        	webView.getSettings().setBuiltInZoomControls(true);
         	webView.getSettings().setUseWideViewPort(true);
         	webView.setVerticalScrollbarOverlay(true);
 //        	webView.setInitialScale(100);
